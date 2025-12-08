00bd2f83f8e25fafc7f264d0e0814bb9373769f6
Szysz-ka <178914485+Szysz-ka@users.noreply.github.com>
2025-12-08 13:48:14 +0100

    Main file. Turns out this script was much easier to write than i anticipated.

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-def matricer(oneSet: list, zeroSet: list):
+def intToBinStr(bits:int, num: int):
+    return "0"*(bits-len(bin(num)[2:]))+bin(num)[2:]
+
+
+def matricer(bits: int, oneSet: list, zeroSet: list):
+    cubesT = [intToBinStr(bits, i) for i in sorted(oneSet)]
+
+    cubesF = [intToBinStr(bits, i) for i in sorted(zeroSet)]
+
+    return [cubesT, cubesF]
+
+print(matricer(4, [0, 1, 2, 3], [4,7,16]))
